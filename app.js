395be5fb70f5df69212e1834a849d0429940fcd8
sdpa <sdpa14@gmail.com>
2021-03-19 20:07:37 -0500

    added necessary libraries

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var jwt = require('jsonwebtoken')
+var jwt = require("jsonwebtoken");
 
 var cors = require("cors");
 var app = express();
 app.use(cors());
 
 var loginRouter = require("./routes/login");
-var signUpRouter = require('./routes/signUp');
-var fuelQuoteFormRouter = require('./routes/fuel-quote-form');
+var signUpRouter = require("./routes/signUp");
+var fuelQuoteFormRouter = require("./routes/fuel-quote-form");
+var profileRouter = require("./routes/profile");
 
 //view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -23,8 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/login', loginRouter);
-app.use('/fuel-quote-form', fuelQuoteFormRouter);
+app.use("/login", loginRouter);
+app.use("/fuel-quote-form", fuelQuoteFormRouter);
 app.use("/signUp", signUpRouter);
 app.use("/profile", profileRouter);
 
