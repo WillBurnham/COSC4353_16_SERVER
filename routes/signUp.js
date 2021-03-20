e18063a618b644860ae4418aba4c40d039dded44
Will Burnham <burnham.will2020@gmail.com>
2021-03-19 19:52:43 -0500

    fixing merge issues

--- routes/signUp.js
+++ routes/signUp.js
@@ -5,40 +5,6 @@ var { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
 let users = [
-<<<<<<< Updated upstream
-  {
-    email: "user1@test1.com",
-    password: "pass1",
-  },
-  {
-    email: "user2@test2.com",
-    password: "pass2",
-  },
-  {
-    email: "user3@test3.com",
-    password: "pass3",
-  },
-];
-
-router.post(
-  "/",
-  [
-    body("email")
-      .isEmail()
-      .withMessage("Enter valid Emai")
-      .isLength({ max: 25 })
-      .withMessage("max length is 25"),
-    body("password").isLength({ min: 4 }).withMessage("Minimum 4 Characters"),
-  ],
-  async (req, res, next) => {
-    const err = validationResult(req);
-    console.log(err);
-    if (!err.isEmpty()) {
-      return res.status(400).json({ errors: err.array() });
-    }
-    try {
-      //   const hashedPassword = await bcrypt.hash(req.body.email, 10);
-=======
     {
         email: "user1@test1.com",
         password: "pass1"
@@ -71,7 +37,6 @@ router.post(
     }
     try {
         //   const hashedPassword = await bcrypt.hash(req.body.email, 10);
->>>>>>> Stashed changes
       const user = users.find((user) => user.email == req.body.email);
 
       //user existst ==> can't create account
