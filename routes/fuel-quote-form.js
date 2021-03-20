00126631f2dda65e0017d2abe80b41103b9e8976
sdpa <sdpa14@gmail.com>
2021-03-19 22:38:25 -0500

    added authentication for fuel-quote

--- routes/fuel-quote-form.js
+++ routes/fuel-quote-form.js
@@ -1,16 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var { body, validationResult } = require("express-validator");
+const authenticateToken = require("../authenticate");
 
-router.post('/', (req, res, next) => {
-    const {gallons, date} = req.body;
-    // console.log(gallons);
-    // console.log(date);
-    let response = {
-        address: '123 Temporary Address Ln',
-        suggestedPrice: '1000.00',
-        amountDue: '1000.00'
-    };
-    res.json(response);
+router.post("/", authenticateToken, (req, res, next) => {
+  const { gallons, date } = req.body;
+  // console.log(gallons);
+  // console.log(date);
+  let response = {
+    address: "123 Temporary Address Ln",
+    suggestedPrice: "1000.00",
+    amountDue: "1000.00",
+  };
+  res.json(response);
 });
 
 module.exports = router;
--- tests/fuel-quote-form.test.js
+++ tests/fuel-quote-form.test.js
@@ -1,4 +1,4 @@
-const fuelQuoteForm = require('../routes/fuel-quote-form');
+const fuelQuoteForm = require("../routes/fuel-quote-form");
 
 const request = require("supertest");
 const express = require("express");
@@ -7,19 +7,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use("/", fuelQuoteForm);
 
-describe('POST /fuel-quote-form', function() {
-    it('responds with json', function(done) {
-      request(app)
-        .post('/')
-        .send({
-            gallons: 0,
-            date: "09-08-1997"
-        })
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(function(err, res) {
-          if (err) return done(err);
-          return done();
-        });
-    });
+describe("POST /fuel-quote-form", function () {
+  it("responds with json", function (done) {
+    request(app)
+      .post("/")
+      .auth(
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIxQHRlc3QxLmNvbSIsImlhdCI6MTYxNjIwODUwM30.ZATREDprnIMyC2H_4fPVrVmGOdQXzE2Ty0MYklGWKhA",
+        { type: "bearer" }
+      )
+      .send({
+        gallons: 0,
+        date: "09-08-1997",
+      })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
 });
